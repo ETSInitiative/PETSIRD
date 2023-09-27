e3c2f1546fbcf8e1771dc2b1efe73bf43e4551c9
Kris Thielemans <k.thielemans@ucl.ac.uk>
2023-09-28 00:17:09 +0100

    added basic scanner information, timing etc
    C++ example code needs updating

--- python/prd_analysis.py
+++ python/prd_analysis.py
@@ -1,4 +1,5 @@
 import sys
+import numpy
 import prd
 
 
@@ -6,13 +7,20 @@ if __name__ == "__main__":
     with prd.BinaryPrdExperimentReader(sys.stdin.buffer) as reader:
         header = reader.read_header()
         print(f"Subject ID: {header.subject.id}")
-        print(f"Number of detectors: {header.number_of_detectors()}")
+        print(f"Number of detectors: {header.scanner.number_of_detectors()}")
+
+        energy_bin_edges = header.scanner.energy_bin_edges
+        print(f"Energy bin edges: {energy_bin_edges}")
+        energy_mid_points = (energy_bin_edges[:-1] + energy_bin_edges[1:])/2
+        print(f"Energy mid points: {energy_mid_points}")
+
+        coincidence_timing_lut = reader.read_coincidence_timing_lut()
 
         energy_1, energy_2 = 0.0, 0.0
         num_events = 0
         for event in reader.read_events():
-            energy_1 += event.energy_1
-            energy_2 += event.energy_2
+            energy_1 += energy_mid_points[event.energy_1_idx]
+            energy_2 += energy_mid_points[event.energy_2_idx]
             num_events += 1
 
         print(f"Number of events: {num_events}")
--- python/prd_generator.py
+++ python/prd_generator.py
@@ -1,43 +1,66 @@
 import sys
 import math
 import random
+import numpy
 
 import prd
 
 # these are constants for now
-NUMBER_OF_ENERGY_BINS = 100
+NUMBER_OF_ENERGY_BINS = 3
 NUMBER_OF_TOF_BINS = 300
+RADIUS = 400
+NUMBER_OF_TIME_FRAMES = 6
+NUMBER_OF_EVENTS = 1000
 
-
-def get_header():
-    offsets = [0.0, 1.0, 2.0]
+# single ring as example
+def get_scanner_info():
+    radius = RADIUS
     angles = [2 * math.pi * i / 10 for i in range(10)]
     detectors = [
-        prd.Detector(offset=offset, angle=angle, id=i)
-        for i, (offset, angle) in enumerate(
-            (offset, angle) for offset in offsets for angle in angles
+        prd.Detector(x=x, y=y, z=0, id=i)
+        for i, (x,y) in enumerate(
+            (radius*math.sin(angle), radius*math.cos(angle)) for angle in angles
         )
     ]
+    # TOF info (in mm)
+    tofBinEdges = numpy.linspace(-RADIUS, RADIUS, NUMBER_OF_TOF_BINS+1, dtype='float32')
+    energyBinEdges = numpy.linspace(430, 650, NUMBER_OF_ENERGY_BINS+1, dtype='float32')
+    return prd.ScannerInformation(
+        detectors=detectors,
+        tof_bin_edges=tofBinEdges,
+        tof_resolution=9.4, # in mm
+        energy_bin_edges = energyBinEdges,
+        energy_resolution_at_511 = .11, # as fraction of 511
+    )
 
+def get_header():
     return prd.Header(
         subject=prd.Subject(id="123456"),
         institution=prd.Institution(
             name="Diamond Light Source",
             address="Harwell Science and Innovation Campus, Didcot, Oxfordshire, OX11 0DE, UK",
         ),
-        detectors=detectors,
+        scanner=get_scanner_info(),
     )
 
+def get_coincidence_timing_LUT():
+    # 1 ms timeframes
+    time_frames = [ prd.TimeInterval(start=t, stop=t+1) for t in range(NUMBER_OF_TIME_FRAMES)]
+    lut = numpy.linspace(0, NUMBER_OF_EVENTS, NUMBER_OF_TIME_FRAMES, dtype='uint64')
+    return prd.CoincidenceTimingLUT(
+        time_frames = prd.TimeFrameInformation(time_frames=time_frames),
+        event_numbers_at_time_frame_start = lut,
+        )
 
 def get_events(header: prd.Header):
-    detector_count = header.number_of_detectors()
-    for _ in range(1000):
+    detector_count = header.scanner.number_of_detectors()
+    for _ in range(NUMBER_OF_EVENTS):
         yield prd.CoincidenceEvent(
-            detector_1_id=random.randint(0, detector_count),
-            detector_2_id=random.randint(0, detector_count),
-            energy_1=random.randint(0, NUMBER_OF_ENERGY_BINS),
-            energy_2=random.randint(0, NUMBER_OF_ENERGY_BINS),
-            delta_t=random.randint(0, NUMBER_OF_TOF_BINS),
+            detector_1_id=random.randrange(0, detector_count),
+            detector_2_id=random.randrange(0, detector_count),
+            energy_1_idx=random.randrange(0, NUMBER_OF_ENERGY_BINS),
+            energy_2_idx=random.randrange(0, NUMBER_OF_ENERGY_BINS),
+            tof_idx=random.randrange(0, NUMBER_OF_TOF_BINS),
         )
 
 
@@ -45,4 +68,5 @@ if __name__ == "__main__":
     with prd.BinaryPrdExperimentWriter(sys.stdout.buffer) as writer:
         header = get_header()
         writer.write_header(header)
+        writer.write_coincidence_timing_lut(get_coincidence_timing_LUT())
         writer.write_events(get_events(header))
