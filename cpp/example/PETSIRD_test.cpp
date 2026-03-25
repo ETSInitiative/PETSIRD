2426fc564c03ffd6ee73c2cf74cb150f767e6425
Kris Thielemans <k.thielemans@ucl.ac.uk>
2026-03-25 00:34:11 +0000

    add example files for external usage

---
+++ cpp/example/PETSIRD_test.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "petsird/binary/protocols.h"
+using petsird::binary::PETSIRDReader;
+
+int
+main(int argc, char const* argv[])
+{
+  auto prog_name = argv[0];
+
+  std::string filename;
+  // option processing
+  while (argc > 1 && (strncmp(argv[1], "-", 1) == 0))
+    {
+      if (strcmp(argv[1], "--input") == 0 || strcmp(argv[1], "-i") == 0)
+        {
+          filename = argv[2];
+          ++argv;
+          --argc;
+        }
+      else
+        {
+          std::cerr << "Wrong options\n";
+          return 1;
+        }
+      ++argv;
+      --argc;
+    }
+
+  // Open the file
+  PETSIRDReader reader(filename);
+  petsird::Header header;
+  reader.ReadHeader(header);
+
+  return 0;
+}
