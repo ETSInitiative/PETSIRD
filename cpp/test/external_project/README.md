088e0849980aa47177b1bde36a8eb14b513fea3c
Kris Thielemans@ucl.ac.uk <k.thielemans@ucl.ac.uk>
2026-08-19 14:42:39 +0100

    add test if external C++ project can use PETSIRD

---
+++ cpp/test/external_project/README.md
@@ -0,0 +1,7 @@
+# Test files for using PETSIRD from C++ with CMake
+
+This directory contains test files for a minimal test if
+CMake can find PETSIRD with `find_package`, and if
+The C++ files compile and link.
+
+There is currently no run-time test.
---
+++ cpp/test/external_project/minimal_test.cpp
@@ -0,0 +1,26 @@
+/*
+  Copyright (C) 2026 University College London
+
+  SPDX-License-Identifier: Apache-2.0
+*/
+
+// (un)comment if you want HDF5 or binary output
+#define USE_HDF5
+
+#ifdef USE_HDF5
+#  include "petsird/hdf5/protocols.h"
+using petsird::hdf5::PETSIRDReader;
+#else
+#  include "petsird/binary/protocols.h"
+using petsird::binary::PETSIRDReader;
+#endif
+
+int
+main(int, char const* argv[])
+{
+  PETSIRDReader reader(argv[1]);
+  petsird::Header header;
+  reader.ReadHeader(header);
+
+  return 0;
+}
